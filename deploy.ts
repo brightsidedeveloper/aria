b033ab2db3c851632d4837f2a0e9e1e22a177b2e
Tim <tim@brightsidedeveloper.com>
2024-05-23 18:06:30 -0500

    Updates to deploy

--- deploy.ts
+++ deploy.ts
@@ -1,23 +1,20 @@
-import { exec, spawn } from "child_process"
+import { exec, spawn } from 'child_process'
 
-import fs from "fs"
-import simpleGit from "simple-git"
-import path from "path"
+import fs from 'fs'
+import simpleGit from 'simple-git'
+import path from 'path'
 
-import { createClient } from "@supabase/supabase-js"
-import dotenv from "dotenv"
+import { createClient } from '@supabase/supabase-js'
+import dotenv from 'dotenv'
 
-import { magenta, cyan, redBright, green, blueBright } from "colorette"
+import { magenta, cyan, redBright, green, blueBright } from 'colorette'
 
 // Load environment variables from .env file
 dotenv.config()
 
 const git = simpleGit()
 
-const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_ANON_KEY!
-)
+const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!)
 
 interface BroadcastPayload {
   event: string
@@ -35,35 +32,36 @@ interface Payload {
 const handleCreateFile = async ({ code, path: filePath }: Payload) => {
   if (!code || !filePath) {
     channel.send({
-      event: "create-file",
-      type: "broadcast",
-      payload: { message: "Invalid payload: Missing code or path" },
+      event: 'create-file',
+      type: 'broadcast',
+      payload: { message: 'Invalid payload: Missing code or path' },
     })
     return
   }
 
-  const ensureFolderPath = filePath.split("/").slice(0, -1).join("/")
+  const ensureFolderPath = filePath.split('/').slice(0, -1).join('/')
   handleCreateFolder({ path: ensureFolderPath })
   try {
     const finalPath = path.join(__dirname, filePath)
-    fs.writeFileSync(finalPath, code, "utf8")
+    fs.writeFileSync(finalPath, code, 'utf8')
     console.log(green(`File created successfully at ${filePath}`))
     channel.send({
-      event: "create-file",
-      type: "broadcast",
+      event: 'create-file',
+      type: 'broadcast',
       payload: {
         message: `File created successfully at ${filePath}`,
         path: filePath,
       },
     })
   } catch (error: unknown) {
-    console.log(redBright("Failed to create file:"), error)
+    console.log(redBright('Failed to create file:'), error)
     channel.send({
-      event: "create-file",
-      type: "broadcast",
+      event: 'create-file',
+      type: 'broadcast',
       payload: {
-        message: `Failed to create file: You are not handling this error well. Look at line 42 of deploy`,
+        message: `Failed to create file`,
         path: filePath,
+        error,
       },
     })
   }
@@ -72,9 +70,9 @@ const handleCreateFile = async ({ code, path: filePath }: Payload) => {
 const handleDeleteFile = async ({ path: filePath }: Payload) => {
   if (!filePath) {
     channel.send({
-      event: "delete-file",
-      type: "broadcast",
-      payload: { message: "Invalid payload: Missing path" },
+      event: 'delete-file',
+      type: 'broadcast',
+      payload: { message: 'Invalid payload: Missing path' },
     })
     return
   }
@@ -85,32 +83,29 @@ const handleDeleteFile = async ({ path: filePath }: Payload) => {
     fs.unlinkSync(finalPath)
     console.log(green(`File deleted successfully from ${filePath}`))
     channel.send({
-      event: "delete-file",
-      type: "broadcast",
+      event: 'delete-file',
+      type: 'broadcast',
       payload: {
         message: `File deleted successfully from ${filePath}`,
         path: filePath,
       },
     })
   } else {
-    console.log(redBright("File does not exist:"), finalPath)
+    console.log(redBright('File does not exist:'), finalPath)
     channel.send({
-      event: "delete-file",
-      type: "broadcast",
-      payload: { message: "File does not exist", path: filePath },
+      event: 'delete-file',
+      type: 'broadcast',
+      payload: { message: 'File does not exist', path: filePath },
     })
   }
 }
 
-const handlePushFile = async ({
-  path: filePath,
-  commitMsg = "Add Changes",
-}: Payload) => {
+const handlePushFile = async ({ path: filePath, commitMsg = 'Add Changes' }: Payload) => {
   if (!filePath) {
     channel.send({
-      event: "push-file",
-      type: "broadcast",
-      payload: { message: "Invalid payload: Missing path" },
+      event: 'push-file',
+      type: 'broadcast',
+      payload: { message: 'Invalid payload: Missing path' },
     })
     return
   }
@@ -119,21 +114,21 @@ const handlePushFile = async ({
   try {
     await git.add(finalPath)
     await git.commit(commitMsg)
-    await git.push("origin", "main") // Ensure 'main' is the correct branch
-    console.log(green("File updated and pushed to GitHub successfully!"))
+    await git.push('origin', 'main') // Ensure 'main' is the correct branch
+    console.log(green('File updated and pushed to GitHub successfully!'))
     channel.send({
-      event: "push-file",
-      type: "broadcast",
+      event: 'push-file',
+      type: 'broadcast',
       payload: {
-        message: "File updated and pushed to GitHub successfully!",
+        message: 'File updated and pushed to GitHub successfully!',
         path: filePath,
       },
     })
   } catch (error) {
-    console.log(redBright("Failed to push file update to GitHub:"), error)
+    console.log(redBright('Failed to push file update to GitHub:'), error)
     channel.send({
-      event: "push-file",
-      type: "broadcast",
+      event: 'push-file',
+      type: 'broadcast',
       payload: {
         message: `Failed to push file update to GitHub: You are not handling this error well. Look at line 42 of deploy`,
         path: filePath,
@@ -145,28 +140,28 @@ const handlePushFile = async ({
 const handleGetFile = async ({ path: filePath }: Payload) => {
   if (!filePath) {
     channel.send({
-      event: "get-file",
-      type: "broadcast",
-      payload: { message: "Invalid payload: Missing path" },
+      event: 'get-file',
+      type: 'broadcast',
+      payload: { message: 'Invalid payload: Missing path' },
     })
     return
   }
 
   const finalPath = path.join(__dirname, filePath)
   if (fs.existsSync(finalPath)) {
-    const code = fs.readFileSync(finalPath, "utf8")
+    const code = fs.readFileSync(finalPath, 'utf8')
     channel.send({
-      event: "return-file",
-      type: "broadcast",
+      event: 'return-file',
+      type: 'broadcast',
       payload: { code, path: filePath },
     })
     console.log(green(`File retrieved successfully from ${filePath}`))
   } else {
-    console.log(redBright("File does not exist:"), finalPath)
+    console.log(redBright('File does not exist:'), finalPath)
     channel.send({
-      event: "return-file",
-      type: "broadcast",
-      payload: { message: "File does not exist", path: filePath },
+      event: 'return-file',
+      type: 'broadcast',
+      payload: { message: 'File does not exist', path: filePath },
     })
   }
 }
@@ -174,9 +169,9 @@ const handleGetFile = async ({ path: filePath }: Payload) => {
 const handleCreateFolder = async ({ path: folderPath }: Payload) => {
   if (!folderPath) {
     channel.send({
-      event: "create-folder",
-      type: "broadcast",
-      payload: { message: "Invalid payload: Missing path" },
+      event: 'create-folder',
+      type: 'broadcast',
+      payload: { message: 'Invalid payload: Missing path' },
     })
     return
   }
@@ -195,7 +190,7 @@ function getAllFiles(dirPath: string, arrayOfFiles: string[] = []): string[] {
     const filePath = path.join(dirPath, file)
 
     // Ignore node_modules and .git folders
-    if (file === "node_modules" || file === ".git") {
+    if (file === 'node_modules' || file === '.git') {
       return
     }
 
@@ -211,34 +206,43 @@ function getAllFiles(dirPath: string, arrayOfFiles: string[] = []): string[] {
 
 const handleGetAllFilePaths = async () => {
   const rootDir = path.resolve(__dirname) // Get the absolute path of the root directory
-  const allFiles = getAllFiles(rootDir)
-  console.log(green("Retrieved all file paths successfully"))
-  channel.send({
-    event: "return-all-files",
-    type: "broadcast",
-    payload: { files: allFiles },
-  })
+  try {
+    const allFiles = getAllFiles(rootDir)
+    console.log(green('Retrieved all file paths successfully'))
+    channel.send({
+      event: 'return-all-files',
+      type: 'broadcast',
+      payload: { files: allFiles },
+    })
+  } catch (error) {
+    console.log(redBright('Failed to get all file paths:'), error)
+    channel.send({
+      event: 'return-all-files',
+      type: 'broadcast',
+      payload: { message: 'Failed to get all file paths', error },
+    })
+  }
 }
 
 let devProcess: ReturnType<typeof spawn> | null = null
 
 const stopDevServer = (fromStart?: boolean) => {
   if (devProcess) {
-    devProcess.kill("SIGINT")
-    console.log("Development server stopped.")
+    devProcess.kill('SIGINT')
+    console.log('Development server stopped.')
     if (!fromStart)
       channel.send({
-        event: "stop-dev-server",
-        type: "broadcast",
-        payload: { message: "Development server stopped" },
+        event: 'stop-dev-server',
+        type: 'broadcast',
+        payload: { message: 'Development server stopped' },
       })
   } else {
-    console.log("No development server is running.")
+    console.log('No development server is running.')
     if (!fromStart)
       channel.send({
-        event: "stop-dev-server",
-        type: "broadcast",
-        payload: { message: "No development server is running" },
+        event: 'stop-dev-server',
+        type: 'broadcast',
+        payload: { message: 'No development server is running' },
       })
   }
 }
@@ -246,32 +250,41 @@ const stopDevServer = (fromStart?: boolean) => {
 const startDevServer = ({ path: filePath }: Payload) => {
   if (!filePath) {
     channel.send({
-      event: "start-dev-server",
-      type: "broadcast",
-      payload: { message: "Invalid payload: Missing path" },
+      event: 'start-dev-server',
+      type: 'broadcast',
+      payload: { message: 'Invalid payload: Missing path' },
     })
     return
   }
   if (devProcess) stopDevServer(true)
   const projectDir = path.join(__dirname, filePath)
-  devProcess = spawn("npm", ["run", "dev"], {
-    cwd: projectDir,
-    stdio: "inherit",
-  })
-  console.log("Development server started...")
-  channel.send({
-    event: "start-dev-server",
-    type: "broadcast",
-    payload: { message: "Development server started" },
-  })
+  try {
+    devProcess = spawn('npm', ['run', 'dev'], {
+      cwd: projectDir,
+      stdio: 'inherit',
+    })
+    console.log('Development server started...')
+    channel.send({
+      event: 'start-dev-server',
+      type: 'broadcast',
+      payload: { message: 'Development server started' },
+    })
+  } catch (error) {
+    console.error('Error starting development server:', error)
+    channel.send({
+      event: 'start-dev-server',
+      type: 'broadcast',
+      payload: { message: 'Error starting development server', error },
+    })
+  }
 }
 
 const runCommand = ({ command, path: filePath }: Payload) => {
   if (!command || !filePath) {
     channel.send({
-      event: "run-command",
-      type: "broadcast",
-      payload: { message: "Invalid payload: Missing command or path" },
+      event: 'run-command',
+      type: 'broadcast',
+      payload: { message: 'Invalid payload: Missing command or path' },
     })
     return
   }
@@ -281,25 +294,25 @@ const runCommand = ({ command, path: filePath }: Payload) => {
     if (error) {
       console.error(`Error: ${error.message}`)
       channel.send({
-        event: "run-command",
-        type: "broadcast",
-        payload: { message: "Error running exec: " + error.message },
+        event: 'run-command',
+        type: 'broadcast',
+        payload: { message: 'Error running exec', error },
       })
       return
     }
     if (stderr) {
       console.error(`Stderr: ${stderr}`)
       channel.send({
-        event: "run-command",
-        type: "broadcast",
-        payload: { message: "Error in stdout: " + stderr },
+        event: 'run-command',
+        type: 'broadcast',
+        payload: { message: 'Error in stderr', stderr },
       })
       return
     }
     console.log(`Terminal Output: ${stdout}`)
     channel.send({
-      event: "run-command",
-      type: "broadcast",
+      event: 'run-command',
+      type: 'broadcast',
       payload: { message: stdout },
     })
   })
@@ -308,35 +321,35 @@ const runCommand = ({ command, path: filePath }: Payload) => {
 const handleBroadcast = async ({ payload, event }: BroadcastPayload) => {
   console.log(blueBright(JSON.stringify({ payload, event })))
   switch (event) {
-    case "create-file":
+    case 'create-file':
       return handleCreateFile(payload)
-    case "delete-file":
+    case 'delete-file':
       return handleDeleteFile(payload)
-    case "push-file":
+    case 'push-file':
       return handlePushFile(payload)
-    case "get-file":
+    case 'get-file':
       return handleGetFile(payload)
-    case "get-all-files":
+    case 'get-all-files':
       return handleGetAllFilePaths()
-    case "start-dev-server":
+    case 'start-dev-server':
       return startDevServer(payload)
-    case "stop-dev-server":
+    case 'stop-dev-server':
       return stopDevServer()
-    case "run-command":
+    case 'run-command':
       return runCommand(payload)
     default:
-      console.log(redBright("Invalid event:"), event)
+      console.log(redBright('Invalid event:'), event)
       channel.send({
-        event: "invalid-event",
-        type: "broadcast",
-        payload: { message: "Invalid event" },
+        event: 'invalid-event',
+        type: 'broadcast',
+        payload: { message: 'Invalid event' },
       })
   }
 }
 
-const channel = supabase.channel("aria")
+const channel = supabase.channel('aria')
 
-channel.on("broadcast", { event: "*" }, handleBroadcast).subscribe()
+channel.on('broadcast', { event: '*' }, handleBroadcast).subscribe()
 
 const logAriaGPT = () => {
   console.log(
