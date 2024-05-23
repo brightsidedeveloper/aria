34f29fbc793e03eb1c22dd575189dd7e76397dba
Tim <tim@brightsidedeveloper.com>
2024-05-22 20:02:55 -0500

    Add generated file

--- GetAllFiles.ts
+++ GetAllFiles.ts
@@ -2,21 +2,21 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 function getAllFiles(dirPath: string, arrayOfFiles: string[] = []): string[] {
-  const files = fs.readdirSync(dirPath);
+    const files = fs.readdirSync(dirPath);
 
-  files.forEach(file => {
-    const fullPath = path.join(dirPath, file);
-    if (fs.statSync(fullPath).isDirectory()) {
-      getAllFiles(fullPath, arrayOfFiles);
-    } else {
-      arrayOfFiles.push(fullPath);
-    }
-  });
+    files.forEach(file => {
+        const filePath = path.join(dirPath, file);
+        if (fs.statSync(filePath).isDirectory()) {
+            arrayOfFiles = getAllFiles(filePath, arrayOfFiles);
+        } else {
+            arrayOfFiles.push(filePath);
+        }
+    });
 
-  return arrayOfFiles;
+    return arrayOfFiles;
 }
 
-const rootDir = path.resolve(__dirname);
+const rootDir = path.resolve(__dirname); // Get the absolute path of the root directory
 const allFiles = getAllFiles(rootDir);
 
 console.log(allFiles);
