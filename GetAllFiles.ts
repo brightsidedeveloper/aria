1dc429f9af07fc806e31cb314d54c3c407998749
Tim <tim@brightsidedeveloper.com>
2024-05-22 20:01:02 -0500

    Add generated file

---
+++ GetAllFiles.ts
@@ -0,0 +1,22 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+function getAllFiles(dirPath: string, arrayOfFiles: string[] = []): string[] {
+  const files = fs.readdirSync(dirPath);
+
+  files.forEach(file => {
+    const fullPath = path.join(dirPath, file);
+    if (fs.statSync(fullPath).isDirectory()) {
+      getAllFiles(fullPath, arrayOfFiles);
+    } else {
+      arrayOfFiles.push(fullPath);
+    }
+  });
+
+  return arrayOfFiles;
+}
+
+const rootDir = path.resolve(__dirname);
+const allFiles = getAllFiles(rootDir);
+
+console.log(allFiles);
