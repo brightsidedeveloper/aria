054c53f8456f6333fdcd8272f89359547e7402dd
Tim <tim@brightsidedeveloper.com>
2024-05-23 18:38:20 -0500

    Update App.tsx to toggle between Intro and Outro components every 5 seconds

--- project-1/src/App.tsx
+++ project-1/src/App.tsx
@@ -1,5 +1,23 @@
-import Intro from './components/Intro'
+import React, { useState, useEffect } from 'react';
+import Intro from './components/Intro';
+import Outro from './components/Outro';
 
-export default function App() {
-  return <Intro />
+const App: React.FC = () => {
+  const [showIntro, setShowIntro] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setShowIntro((prevShowIntro) => !prevShowIntro);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      {showIntro ? <Intro /> : <Outro />}
+    </div>
+  );
 }
+
+export default App;
