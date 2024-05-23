fd101c2802d56edec3c92cdcfeb6efb7f536cb8c
Tim <tim@brightsidedeveloper.com>
2024-05-22 21:49:08 -0500

    Updated App.tsx to center content, make text red and 7xl

--- project-1/src/App.tsx
+++ project-1/src/App.tsx
@@ -1,3 +1,9 @@
 export default function App() {
-  return <div>App</div>
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <p className="text-red-500 text-7xl">
+        App
+      </p>
+    </div>
+  )
 }
