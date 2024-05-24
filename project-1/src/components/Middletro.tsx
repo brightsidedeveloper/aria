b63aad560f43992801645401eead0091ce994e26
Tim <timvan0118@gmail.com>
2024-05-24 12:41:52 -0500

    Added Middletro component with custom message

---
+++ project-1/src/components/Middletro.tsx
@@ -0,0 +1,8 @@
+export default function Middletro() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen bg-slate-900'>
+      <h1 className='text-7xl fade-in font-bold text-green-500 shadow-inner'>This is awesome</h1>
+      <div className='mt-8 fade-in text-xl text-white'>Obviously it's awesome, Tim made it!</div>
+    </div>
+  )
+}
