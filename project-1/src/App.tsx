e79d17a6e315f691ff0e3ec9f31f18c56a8a9e1a
Tim <timvan0118@gmail.com>
2024-05-24 12:52:01 -0500

    Save Revert

--- project-1/src/App.tsx
+++ project-1/src/App.tsx
@@ -1,29 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import Intro from './components/Intro';
-import Outro from './components/Outro';
-import Middletro from './components/Middletro';
+import React, { useState, useEffect } from "react"
+import Intro from "./components/Intro"
+import Outro from "./components/Outro"
 
 const App: React.FC = () => {
-  const [currentComponent, setCurrentComponent] = useState('Intro');
+  const [currentComponent, setCurrentComponent] = useState("Intro")
 
   useEffect(() => {
-    const components = ['Intro', 'Middletro', 'Outro'];
-    let index = 0;
+    const components = ["Intro", "Middletro", "Outro"]
+    let index = 0
     const interval = setInterval(() => {
-      index = (index + 1) % components.length;
-      setCurrentComponent(components[index]);
-    }, 5000);
+      index = (index + 1) % components.length
+      setCurrentComponent(components[index])
+    }, 5000)
 
-    return () => clearInterval(interval);
-  }, []);
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <div>
-      {currentComponent === 'Intro' && <Intro />}
-      {currentComponent === 'Middletro' && <Middletro />}
-      {currentComponent === 'Outro' && <Outro />}
+      {currentComponent === "Intro" && <Intro />}
+      {currentComponent === "Outro" && <Outro />}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
